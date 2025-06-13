4246f1f9ad3101d7d0f53fa8cedd571cdab235f4
Fer <fer.falcon.dg@gmail.com>
2025-06-13 17:43:34 -0300

    front page

---
+++ wp-content/themes/planets/front-page.php
@@ -0,0 +1,30 @@
+<?php
+get_header();
+?>
+
+<main class="planet-list">
+  <h1>Planets of the Solar System</h1>
+  <div class="planet-grid">
+  <?php
+  $planets = new WP_Query([
+    'post_type' => 'planet',
+    'posts_per_page' => -1,
+    'orderby' => 'menu_order',
+    'order' => 'ASC'
+  ]);
+
+  if ($planets->have_posts()) :
+    while ($planets->have_posts()) : $planets->the_post();
+  ?>
+    <article class="planet-card">
+      <a href="<?php the_permalink(); ?>">
+        <?php the_post_thumbnail('medium'); ?>
+        <h2><?php the_title(); ?></h2>
+      </a>
+    </article>
+  <?php endwhile; wp_reset_postdata(); endif; ?>
+  </div>
+</main>
+
+<?php
+get_footer();
